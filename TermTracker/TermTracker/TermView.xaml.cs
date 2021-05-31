4617843c93c67f9e2bf89d5120cd4007b52e27a6
Christopher Gambrell <waymer147@gmail.com>
2021-05-30 22:27:28 -0400

    Added Logic For TermView

--- TermTracker/TermTracker/TermView.xaml.cs
+++ TermTracker/TermTracker/TermView.xaml.cs
@@ -1,15 +1,49 @@
 ﻿
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+using System;
 
 namespace TermTracker
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TermView : ContentPage
     {
-        public TermView()
+        ObservableCollection<Course> courses = new ObservableCollection<Course>();
+
+        public ObservableCollection<Course> Courses { get { return courses; } }
+
+        public TermView(ref Term term)
         {
             InitializeComponent();
+            
+            if(term.Courses.Count == 0)
+            {
+                PopulateTermWithCourses(ref term);
+                
+            }
+
+            coursesListLabel.Text = $"Courses ({term.DisplayName})";
+            CourseList.ItemsSource = term.Courses;
+        }
+        
+        private void courseLvItemTapped (object sender, ItemTappedEventArgs e)
+        {
+
+        }
+        private void OnClickAddCourse(object sender, EventArgs e)
+        {
+
+        }
+        private void OnClickReload(object sender, EventArgs e)
+        {
+
+        }
+        private void PopulateTermWithCourses(ref Term term)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                term.Courses.Add(new Course("Course", DateTime.Now, DateTime.Now.AddDays(90)));
+            }
         }
     }
 }
