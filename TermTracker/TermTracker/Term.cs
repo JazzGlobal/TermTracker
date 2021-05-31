031b155a5f3cfd242dadb099079c6e623daf6600
Christopher Gambrell <waymer147@gmail.com>
2021-05-30 21:52:45 -0400

    Create Term.cs
    Moved Term class to its own file.

---
+++ TermTracker/TermTracker/Term.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TermTracker
+{
+    public class Term
+    {
+        public string DisplayName { get; set; }
+        public DateTime TermStart { get; set; }
+        public DateTime TermEnd { get; }
+
+        public string FormattedTermTitle { get { return $"{DisplayName}\n{TermStart.ToString("MM-dd-yyyy")} - {TermEnd.ToString("MM-dd-yyyy")}"; } }
+
+        public Term(string displayName, DateTime termStart)
+        {
+            DisplayName = displayName;
+            TermStart = termStart;
+            TermEnd = termStart.AddMonths(6);
+        }
+
+        public Term()
+        {
+        }
+    }
+}
