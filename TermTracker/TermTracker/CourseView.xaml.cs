5038ba2809ca55523df6d5681b57f687a15cb37a
Christopher Gambrell <waymer147@gmail.com>
2021-05-31 18:43:00 -0400

    Created

---
+++ TermTracker/TermTracker/CourseView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TermTracker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CourseView : ContentPage
+    {
+        public CourseView(Course course)
+        {
+            InitializeComponent();
+            courseNameLabel.Text = course.CourseName;
+            courseDurationLabel.Text = $"{course.CourseStart} - {course.CourseEnd}";
+            courseInstructorValue.Text = course.Instructor;
+            courseStatusValue.Text = course.Status.ToString();
+            courseNotesValue.Text = course.Notes;
+        }
+    }
+}
