3d25fe3e0bb1cc5c64a7c0c6ca10f68657cf12d0
Christopher Gambrell <waymer147@gmail.com>
2021-05-30 21:53:03 -0400

    Added TermEdit
    Added term edit form.

---
+++ TermTracker/TermTracker/TermEdit.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TermTracker
+{
+    // [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TermEdit : ContentPage
+    {
+        Term term; 
+        public TermEdit(ref Term term)
+        {
+            InitializeComponent();
+            this.term = term;
+            if (term.DisplayName.Trim() != "" && term.DisplayName != null)
+            {
+                termNameLabel.Text = term.DisplayName;
+                termNameInput.Text = term.DisplayName;
+            }
+        }
+
+        void OnDateSelected(object sender, DateChangedEventArgs args)
+        {
+            var picker = (DatePicker)sender;
+            Debug.WriteLine(picker.Date);
+        }
+
+        void OnSubmitButtonClicked (object sender, EventArgs args)
+        {
+            Debug.WriteLine("Submit button clicked!");
+            if(termNameInput.Text.Trim() != "" && termNameInput.Text != null)
+            {
+                term.DisplayName = termNameInput.Text;
+                term.TermStart = termStartDateInput.Date;
+            }
+            // TODO: Save current term name and start date to database.
+            Navigation.PopAsync();
+        }
+    }
+}
