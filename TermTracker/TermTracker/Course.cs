2104e7c85c1db632b9f86adc1a8f8fec1713d6cf
Christopher Gambrell <waymer147@gmail.com>
2021-05-30 22:06:41 -0400

    Modified Property Names

--- TermTracker/TermTracker/Course.cs
+++ TermTracker/TermTracker/Course.cs
@@ -5,14 +5,15 @@ namespace TermTracker
     public class Course
     {
         public string CourseName { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime CourseStart { get; set; }
+        public DateTime CourseEnd { get; set; }
+        public string FormattedCourseTitle { get { return $"{CourseName}\n{CourseStart.ToString("MM-dd-yyyy")} - {CourseEnd.ToString("MM-dd-yyyy")}"; } }
 
         public Course(string courseName, DateTime startDate, DateTime endDate)
         {
             CourseName = courseName;
-            StartDate = startDate;
-            EndDate = endDate;
+            CourseStart = startDate;
+            CourseEnd = endDate;
         }
     }
 }
