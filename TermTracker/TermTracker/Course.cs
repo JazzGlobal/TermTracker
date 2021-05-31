bc46f9694e05b23d4b79caac27fd139263382791
Christopher Gambrell <waymer147@gmail.com>
2021-05-30 22:02:37 -0400

    Create Course.cs

---
+++ TermTracker/TermTracker/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TermTracker
+{
+    public class Course
+    {
+        public string CourseName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Course(string courseName, DateTime startDate, DateTime endDate)
+        {
+            CourseName = courseName;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
