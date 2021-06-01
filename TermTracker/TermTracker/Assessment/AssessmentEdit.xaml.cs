83cf6d03a1fe81fb0d652ce7493cb9b1d7267952
Christopher Gambrell <waymer147@gmail.com>
2021-06-01 19:07:41 -0400

    Added Assessment Forms

---
+++ TermTracker/TermTracker/Assessment/AssessmentEdit.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TermTracker.Assessment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AssessmentEdit : ContentPage
+    {
+        public AssessmentEdit(ref Assessment assessment)
+        {
+            InitializeComponent();
+            assessmentNameValue.Text = assessment.AssessmentName;
+            assessmentStartDateValue.Date = assessment.AssessmentStart;
+            assessmentEndDateValue.Date = assessment.AssessmentEnd;
+
+            assessmentTypeValuePicker.Items.Add(Assessment.AssessmentType.Objective.ToString());
+            assessmentTypeValuePicker.Items.Add(Assessment.AssessmentType.Performance.ToString());
+            assessmentTypeValuePicker.SelectedIndex = (int)assessment.Type;
+        }
+
+        private void OnSaveButtonClicked(object sender, EventArgs args)
+        {
+            Debug.WriteLine("Save Assessment Details Button Clicked!");
+        }
+    }
+}
---
+++ TermTracker/TermTracker/Assessment/AssessmentView.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TermTracker.Assessment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AssessmentView : ContentPage
+    {
+        public AssessmentView(Assessment assessment)
+        {
+            InitializeComponent();
+            assessmentNameLabel.Text = assessment.AssessmentName;
+            assessmentDurationLabel.Text = $"{assessment.AssessmentStart.ToString("MM-dd-yyyy")} - {assessment.AssessmentEnd.ToString("MM-dd-yyyy")}";
+            assessmentTypeValue.Text = assessment.Type.ToString();
+        }
+    }
+}
