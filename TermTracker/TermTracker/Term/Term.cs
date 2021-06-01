2abf0d8696c9d536281e3058f3f89ade2db7b0ca
Christopher Gambrell <waymer147@gmail.com>
2021-06-01 19:08:42 -0400

    Refactored with INotifyPropertyChanged implemented.

--- TermTracker/TermTracker/Term/Term.cs
+++ TermTracker/TermTracker/Term/Term.cs
@@ -1,19 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace TermTracker
 {
-    public class Term
+    public class Term : INotifyPropertyChanged
     {
-        public string DisplayName { get; set; }
-        public DateTime TermStart { get; set; }
-        public DateTime TermEnd { get; }
-        public List<Course> Courses { get; set; }
+        private string displayName;
+        public string DisplayName
+        {
+            get { return displayName; }
+            set
+            {
+                displayName = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("DisplayName"));
+            }
+        }
+        private DateTime termStart;
+        public DateTime TermStart
+        {
+            get { return termStart; }
+            set
+            {
+                termStart = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("TermStart"));
+            }
+        }
+        private DateTime termEnd;
+        public DateTime TermEnd
+        {
+            get { return termEnd; }
+            set
+            {
+                termEnd = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("TermEnd"));
+            }
+        }
+        private List<Course> courses;
+        public List<Course> Courses
+        {
+            get { return courses; }
+            set
+            {
+                courses = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Courses"));
+            }
+        }
         public string FormattedTermTitle { get { return $"{DisplayName}\n{TermStart.ToString("MM-dd-yyyy")} - {TermEnd.ToString("MM-dd-yyyy")}"; } }
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
 
         public Term(string displayName, DateTime termStart)
         {
-            DisplayName = displayName;
+            this.displayName = displayName;
             TermStart = termStart;
             TermEnd = termStart.AddMonths(6);
             Courses = new List<Course>();
@@ -30,5 +68,6 @@ namespace TermTracker
         {
 
         }
+
     }
 }
