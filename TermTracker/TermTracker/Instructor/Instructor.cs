ae510caab596bb220de43e1e1cbad95f04f81195
Christopher Gambrell <waymer147@gmail.com>
2021-06-01 19:08:17 -0400

    Create Instructor.cs

---
+++ TermTracker/TermTracker/Instructor/Instructor.cs
@@ -0,0 +1,32 @@
+﻿namespace TermTracker.Instructor
+{
+    public class Instructor
+    {
+        private string name;
+        private string phoneNumber;
+        private string email;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public Instructor(string name, string phoneNumber, string email)
+        {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+    }
+}
