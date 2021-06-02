80501d706d2888e97a0e6bf3bb70218ea0777e94
Christopher Gambrell <waymer147@gmail.com>
2021-06-01 23:12:44 -0400

    Added InstructorEdit

---
+++ TermTracker/TermTracker/Instructor/InstructorEdit.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TermTracker.Instructor
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InstructorEdit : ContentPage
+    {
+        Instructor instructor;
+        public InstructorEdit(ref Instructor instructor)
+        {
+            this.instructor = instructor;
+            InitializeComponent();
+
+            instructorNameValue.Text = instructor.Name;
+            instructorEmailValue.Text = instructor.Email;
+            instructorPhoneNumberValue.Text = instructor.PhoneNumber;
+        }
+
+        private void OnSaveButtonClicked(object sender, EventArgs args)
+        {
+            instructor.Name = instructorNameValue.Text;
+            instructor.PhoneNumber = instructorPhoneNumberValue.Text;
+            instructor.Email = instructorEmailValue.Text;
+            Debug.WriteLine("Save Instructor Button Clicked!");
+        }
+    }
+}
