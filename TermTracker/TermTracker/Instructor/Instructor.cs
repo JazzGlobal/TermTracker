797e4e01b8d26349c0816e3d347158f1809d370a
Christopher Gambrell <waymer147@gmail.com>
2021-06-02 21:40:09 -0400

    Instructor object is now converted to a database object.

--- TermTracker/TermTracker/Instructor/Instructor.cs
+++ TermTracker/TermTracker/Instructor/Instructor.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections.Generic;
-
+using SQLite;
 namespace TermTracker.Instructor
 {
+    [Table("instructors")]
     public class Instructor
     {
         public static List<Instructor> AvailableInstructors = new List<Instructor>();
@@ -9,17 +10,21 @@ namespace TermTracker.Instructor
         private string name;
         private string phoneNumber;
         private string email;
-
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+        [MaxLength(250)]
         public string Name
         {
             get { return name; }
             set { name = value; }
         }
+        [MaxLength(250)]
         public string PhoneNumber
         {
             get { return phoneNumber; }
             set { phoneNumber = value; }
         }
+        [MaxLength(250)]
         public string Email
         {
             get { return email; }
@@ -31,6 +36,20 @@ namespace TermTracker.Instructor
             this.name = name;
             this.phoneNumber = phoneNumber;
             this.email = email;
+        }
+        public Instructor()
+        {
+
+        }
+        public static List<Instructor> GetAllInstructors(SQLite.SQLiteConnection conn)
+        {
+            List<Instructor> gen_instructors = conn.Table<Instructor>().ToList();
+            return gen_instructors;
+        }
+        public static int AddNewInstructor(SQLite.SQLiteConnection conn, Instructor instructor)
+        {
+            int result = conn.Insert(instructor);
+            return result;
         }
     }
 }
